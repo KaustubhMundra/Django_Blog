3f06bae88c1a68e658a11252d22779a400670a81
Kaustubh Mundra <kaustubh.mundra3@gmail.com>
2016-09-23 03:20:27 +0530

    Pagination
    Multiple pages an dynamic url’s for pages

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,9 +1,13 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.shortcuts import render
+
 from django.contrib import messages
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404,redirect
 
 from .forms import PostForm
 from .models import Post
+
 
 def post_create(request):
 	form = PostForm(request.POST or None)
@@ -29,10 +33,23 @@ def post_detail(request, id=None):
 	return render(request, "post_detail.html", context)
 
 def post_list(request):
-	queryset = Post.objects.all()
+	queryset_list = Post.objects.all()#.order_by("-timestamp")
+	paginator = Paginator(queryset_list, 3) # Show 25 contacts per page
+	page_request_var="abc"
+	page = request.GET.get(page_request_var)
+	try:
+		queryset = paginator.page(page)
+	except PageNotAnInteger:
+		# If page is not an integer, deliver first page.
+		queryset = paginator.page(1)
+	except EmptyPage:
+	# If page is out of range (e.g. 9999), deliver last page of results.
+		queryset = paginator.page(paginator.num_pages)
+
 	context = {
 		"object_list": queryset, 
-		"title": "List"
+		"title": "List",
+		"page_request_var": page_request_var
 	}
 	return render(request, "post_list.html", context)
 
@@ -52,6 +69,7 @@ def post_update(request, id=None):
 		"title": instance.title,
 		"instance": instance,
 		"form":form,
+
 	}
 	return render(request, "post_form.html", context)
 
@@ -62,3 +80,9 @@ def post_delete(request,id=None):
 	instance.delete()
 	messages.success(request, "Successfully Deleted" )
 	return redirect("posts:list")
+
+
+
+
+
+    
