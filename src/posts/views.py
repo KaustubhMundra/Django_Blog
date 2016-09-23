f24aea8a0d0fba39bb0abde6d29f8a20e7b19a85
Kaustubh Mundra <kaustubh.mundra3@gmail.com>
2016-09-23 10:01:09 +0530

    Author title
    Author name from author id

--- src/posts/views.py
+++ src/posts/views.py
@@ -3,7 +3,7 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
 
 from django.contrib import messages
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render, get_object_or_404,redirect
 
 from .forms import PostForm
@@ -11,6 +11,8 @@ from .models import Post
 
 
 def post_create(request):
+	if not request.user.is_staff or not request.user.is_superuser:
+		raise Http404
 	form = PostForm(request.POST or None,request.FILES or None)
 	if form.is_valid():
 		instance = form.save(commit=False)
@@ -58,12 +60,17 @@ def post_list(request):
 
 
 def post_update(request, slug=None):
+	if not request.user.is_staff or not request.user.is_superuser:
+		raise Http404
+	if not request.user.is_authenticated :
+		raise Http404	
 	instance = get_object_or_404(Post, slug=slug)
 	form = PostForm(request.POST or None, request.FILES or None,instance=instance,)
 	if form.is_valid():
 		instance = form.save(commit=False)
+		instance.user=request.user
 		instance.save()
-		messages.success(request, "<a href='#'>Successfully</a> Updated", extra_tags='' )
+		messages.success(request, "<a href='#'>Successfully</a> Updated", extra_tags='html_safe' )
 		return HttpResponseRedirect(instance.get_absolute_url())
 
 
@@ -79,6 +86,8 @@ def post_update(request, slug=None):
 
 
 def post_delete(request,slug=None):
+	if not request.user.is_staff or not request.user.is_superuser:
+		raise Http404
 	instance = get_object_or_404(Post, slug=slug)
 	instance.delete()
 	messages.success(request, "Successfully Deleted" )
